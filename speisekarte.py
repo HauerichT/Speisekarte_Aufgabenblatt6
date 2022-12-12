eff585b8cb3c033863420df98069aec04b6094a7
timohaverich <timo.haverich@fh-bielefeld.de>
2022-12-12 22:49:13 +0100

    Fin

--- speisekarte.py
+++ speisekarte.py
@@ -7,6 +7,7 @@ from os import path
 
 # Deklaration der Speisekarte als dictionary
 speisekarte = {}
+kategorien = ["Vorspeisen", "Hauptspeisen", "Nachspeisen", "Getränke"]
 
 # Speicherung des Dateipfades der Speisekarte
 ordnerPfad = path.dirname(__file__)
@@ -19,74 +20,19 @@ def speisekarteAnzeigen(karte):
     if len(karte) > 0:
 
         print("\n----- Speisekarte -----")
-
-        # printet die Vorspeisen
-        print("Vorspeisen:")
-        vorspeiseExists = False
-        for id, info in karte.items():
-            for key in info:
-                if key == "kategorie" and info[key] == "Vorspeise":
+        print(len(karte))
+        foundKat = False
+        for kat in kategorien:
+            print(kat + ": ")
+            for id, info in karte.items():
+                if info["kategorie"] == kat:
                     print(str(id) + ": " + info["gericht"] + ", " + info["preis"] + "ct")
-                    vorspeiseExists = True
-        if not vorspeiseExists:
-            print("Noch keine Vorspeise hinzugefügt.")
-
-        print()
-
-        # printet die Hauptspeisen
-        print("Hauptspeisen:")
-        hauptspeiseExists = False
-        for id, info in karte.items():
-            for key in info:
-                if key == "kategorie" and info[key] == "Hauptspeise":
-                    print(str(id) + ": " + info["gericht"] + ", " + info["preis"] + "ct")
-                    hauptspeiseExists = True
-        if not hauptspeiseExists:
-            print("Noch keine Hauptspeise hinzugefügt.\n")
-
-        print()
-
-        # printet die Nachspeisen
-        print("Nachspeisen:")
-        nachspeiseExists = False
-        for id, info in karte.items():
-            for key in info:
-                if key == "kategorie" and info[key] == "Nachspeise":
-                    print(str(id) + ": " + info["gericht"] + ", " + info["preis"] + "ct")
-                    nachspeiseExists = True
-        if not nachspeiseExists:
-            print("Noch keine Nachspeise hinzugefügt.\n")
-
-        print()
-
-        # printet die Getränke
-        print("Getränke:")
-        getraenkExists = False
-        for id, info in karte.items():
-            for key in info:
-                if key == "kategorie" and info[key] == "Getränk":
-                    print(str(id) + ": " + info["gericht"] + ", " + info["preis"] + "ct")
-                    getraenkExists = True
-        if not getraenkExists:
-            print("Noch kein Getränk hinzugefügt.\n")
+                    foundKat = True
+            if not foundKat:
+                print("Noch kein Gericht in dieser Kategorie.")
 
     else:
         print("\nDie Speisekarte ist leer!")
-
-
-def kategorieCheck(karte):
-    # Abfrage des Preises
-    print("1: Vorspeise\n2: Hauptspeise\n3: Nachspeise\n4: Getränk")
-    inputKategorie = input("Nummer der Kategorie: ")
-
-    if inputKategorie == "":
-        return ""
-
-    if inputKategorie == "1" or inputKategorie == "2" or inputKategorie == "3" or inputKategorie == "4":
-        return inputKategorie
-    else:
-        print("Keine bestehende Kategorie gewählt!\n")
-        kategorieCheck(karte)
 
 
 # Methode um ein Gericht zur Speisekarte hinzuzufügen
@@ -103,26 +49,42 @@ def gerichtHinzufuegen(karte):
         if neuesGerichtPreis == "":
             pass
         else:
-            neuesGerichtKategorie = kategorieCheck(karte)
-            if neuesGerichtKategorie == "":
-                pass
-            else:
-                print("super")
-    return karte
+            while True:
+                print("1: Vorspeisen\n2: Hauptspeisen\n3: Nachspeisen\n4: Getränke")
+                neuesGerichtKategorie = input("Nummer der Kategorie: ")
 
+                if neuesGerichtKategorie == "":
+                    return karte
+
+                if neuesGerichtKategorie != "1" and neuesGerichtKategorie != "2" and neuesGerichtKategorie != "3" and neuesGerichtKategorie != "4":
+                    print("Keine bestehende Kategorie gewählt!\n")
+                else:
+                    lastKey = next(reversed(karte.keys()))
+                    newItemKey = lastKey+1
+                    karte[newItemKey] = {}
+                    karte[newItemKey]["gericht"] = neuesGericht
+                    karte[newItemKey]["preis"] = neuesGerichtPreis
+                    karte[newItemKey]["kategorie"] = kategorien[int(neuesGerichtKategorie)-1]
+
+                    return karte
 
 # Methode um ein Gericht aus der Speisekarte zu löschen
 def gerichtLoeschen(karte):
     if len(karte) > 0:
-        speisekarteAnzeigen(karte)
-        # fragt das zu löschende Gericht ab
-        eingabeNummer = input("\nNummer des zu löschenden Elements eingeben: ")
 
-        for id, info in karte.items():
-            if id == int(eingabeNummer):
-                del karte[id]
-                print("Gericht gelöscht!")
+        while True:
+            speisekarteAnzeigen(karte)
+            deletInput = input("\nNummer des zu löschenden Elements eingeben: ")
+
+            if deletInput == "":
                 return karte
+
+            for id, info in karte.items():
+                if str(id) == deletInput:
+                    del karte[id]
+                    print(info["gericht"] + " erfolgreich gelöscht!")
+                    return karte
+            print("Kein Gericht mit der eingegebenen Nummer gefunden!")
 
     else:
         print("\nDie Speisekarte ist leer!")
@@ -160,18 +122,26 @@ def dateiLesen(karte, pfad):
             if not i.isspace():
                 # entfernt \n am Ende einer Zeile
                 i = i.rstrip()
-                # teilt jede Zeile in gericht und preis wo ein "," ist
+                # teilt jede Zeile in gericht und preis, wo ein "," ist
                 (gericht, preis, kategorie) = i.split(",")
                 # entfernt das Leerzeichen nach dem Komma
                 preis = preis[1:]
                 kategorie = kategorie[1:]
-                # speichert die Speisekarte im dictionary speisekarte
-                karte[counter] = {}
-                karte[counter]["gericht"] = gericht
-                karte[counter]["preis"] = preis
-                karte[counter]["kategorie"] = kategorie
 
-                counter += 1
+                kategorieValide = False
+                for kat in kategorien:
+                    if kategorie == kat:
+                        kategorieValide = True
+
+                if not kategorieValide:
+                    print(gericht + " hat leider keine valide Kategorie und wird somit nicht berücksichtigt!")
+                else:
+                    # speichert die Speisekarte im dictionary speisekarte
+                    karte[counter] = {}
+                    karte[counter]["gericht"] = gericht
+                    karte[counter]["preis"] = preis
+                    karte[counter]["kategorie"] = kategorie
+                    counter += 1
 
 
 # ruft die Methode dateiLesen auf
